bd7fe964573f65c676b4c3ec75c8d41fd3fd7d80
Dai Bui <dbui@confluent.io>
2019-12-14 11:59:06 -0800

    Fix the seeking issue

--- src/main/java/org/freedesktop/gstreamer/kafka/KafkaConsumerWrapper.java
+++ src/main/java/org/freedesktop/gstreamer/kafka/KafkaConsumerWrapper.java
@@ -1,6 +1,5 @@
 package org.freedesktop.gstreamer.kafka;
 
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -25,18 +24,21 @@ public class KafkaConsumerWrapper {
     consumerProperties.put("zookeeper.session.timeout.ms", "6000");
     consumerProperties.put("zookeeper.sync.time.ms","2000");
     consumerProperties.put("auto.commit.enable", "false");  //manually commit the offset
-    consumerProperties.put("auto.commit.interval.ms", "1000");
+    consumerProperties.put("auto.commit.interval.ms", "10000");
+    consumerProperties.put("max.poll.interval.ms", 10000000);
     consumerProperties.put("consumer.timeout.ms", "-1");
     consumerProperties.put("max.poll.records", "1");
     consumerProperties.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
-    consumerProperties.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+    consumerProperties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
     kafkaConsumer = new KafkaConsumer<>(consumerProperties);
-    kafkaConsumer.subscribe(Arrays.asList(topics));
+//    kafkaConsumer.subscribe(Arrays.asList(topics));
+    kafkaConsumer.assign(Arrays.asList(new TopicPartition(topics, 0)));
+
   }
 
   public ConsumerRecords<String, byte[]> receive() {
-    return kafkaConsumer.poll(Duration.ofMillis(100));
+    return kafkaConsumer.poll(100);
   }
 
   public void seekToEnd(TopicPartition topicPartition) {
@@ -62,6 +64,7 @@ public class KafkaConsumerWrapper {
   public void commitOffset(String topic, int partition, long offset) {
     Map<TopicPartition, OffsetAndMetadata> commitMessage = new HashMap<>();
 
+    System.out.println("Committing offset for topic " + topic + " Parition " + partition + " offset " + offset);
     commitMessage.put(new TopicPartition(topic, partition),
         new OffsetAndMetadata(offset));
 
